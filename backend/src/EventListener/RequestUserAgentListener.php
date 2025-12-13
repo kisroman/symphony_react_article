7d470e251a9d763ea83cd0f21739f19736bbdb83
RomanKis <roman.kis.y@gmail.com>
2025-12-13 13:49:49 +0200

    10. Request Listener

---
+++ backend/src/EventListener/RequestUserAgentListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+final class RequestUserAgentListener
+{
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(private readonly LoggerInterface $logger) {
+
+    }
+
+    /**
+     * @param RequestEvent $event
+     *
+     * @return void
+     */
+    #[AsEventListener]
+    public function onRequestEvent(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $userAgent = $request->headers->get('User-Agent');
+
+        $this->logger->info(sprintf('User-Agent: %s', $userAgent));
+    }
+}
